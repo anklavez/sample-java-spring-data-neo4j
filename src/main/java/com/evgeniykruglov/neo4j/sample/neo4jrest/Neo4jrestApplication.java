167e41a3c37abac2d147419849bc51b292056348
evgeniy-kruglov <evgeniy.kruglov@sofvers.com>
2018-04-27 14:25:18 +0400

    Initial project structure

---
+++ src/main/java/com/evgeniykruglov/neo4j/sample/neo4jrest/Neo4jrestApplication.java
@@ -0,0 +1,12 @@
+package com.evgeniykruglov.neo4j.sample.neo4jrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jrestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/evgeniykruglov/neo4j/sample/neo4jrest/Neo4jrestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.evgeniykruglov.neo4j.sample.neo4jrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Neo4jrestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
